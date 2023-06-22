f1fdfb2dbe1253f4c8ca5fa8dcd0cb6d96bc6166
Lucas Teles <lucas.teles@outlook.com>
2023-06-22 12:26:18 -0300

    add nested id test

--- test/Strongly.Tests/NestedIdTests.cs
+++ test/Strongly.Tests/NestedIdTests.cs
@@ -1,10 +1,13 @@
+using Strongly.IntegrationTests.Types;
+using Xunit;
+
 namespace Strongly.IntegrationTests;
 
 public class NestedIdTests
 {
-    // [Fact]
-    // public void CanCreateNestedId()
-    // {
-    //     var id = SomeType<object>.NestedType<string, int>.MoreNesting.VeryNestedId.New();
-    // }
+    [Fact]
+    public void CanCreateNestedId()
+    {
+        var id = SomeType<object>.NestedType<string, int>.MoreNesting.VeryNestedId.New();
+    }
 }
--- test/Strongly.Tests/Types/DefaultId.cs
+++ test/Strongly.Tests/Types/DefaultId.cs
@@ -7,53 +7,37 @@
 namespace Strongly.IntegrationTests.Types;
 
 [Strongly]
-partial struct DefaultId1
-{
-}
+partial struct DefaultId1 { }
 
 [Strongly]
-public partial struct DefaultId2
-{
-}
+public partial struct DefaultId2 { }
 
 [Strongly(converters: StronglyConverter.None)]
-public partial struct NoConverterDefaultId
-{
-}
+public partial struct NoConverterDefaultId { }
 
 [Strongly(converters: StronglyConverter.TypeConverter)]
-public partial struct NoJsonDefaultId
-{
-}
+public partial struct NoJsonDefaultId { }
 
 [Strongly(converters: StronglyConverter.NewtonsoftJson)]
-public partial struct NewtonsoftJsonDefaultId
-{
-}
+public partial struct NewtonsoftJsonDefaultId { }
 
 [Strongly(converters: StronglyConverter.TypeConverter | StronglyConverter.SystemTextJson)]
-public partial struct SystemTextJsonDefaultId
-{
-}
+public partial struct SystemTextJsonDefaultId { }
 
 [Strongly(converters: StronglyConverter.NewtonsoftJson | StronglyConverter.SystemTextJson)]
-public partial struct BothJsonDefaultId
-{
-}
+public partial struct BothJsonDefaultId { }
 
 [Strongly(converters: StronglyConverter.EfValueConverter)]
-public partial struct EfCoreDefaultId
-{
-}
+public partial struct EfCoreDefaultId { }
 
-// public partial class SomeType<T> where T : new()
-// {
-//     public partial record NestedType<TKey, TValue>
-//     {
-//         public partial struct MoreNesting
-//         {
-//             [Strongly]
-//             public partial struct VeryNestedId {}
-//         }
-//     }
-// }
+public partial class SomeType<T> where T : new()
+{
+    public partial record NestedType<TKey, TValue>
+    {
+        public partial struct MoreNesting
+        {
+            [Strongly]
+            public partial struct VeryNestedId { }
+        }
+    }
+}
